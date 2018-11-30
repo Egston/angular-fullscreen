1e616820f3a05c9e55e364a6e5e20742fa2e58d6
Yurii Malchevskyi <yurii_malchevskyi@epam.com>
2018-11-30 10:24:38 +0200

    CXS-3007: removed temporal fix for safari, fixed default function name
    (cherry picked from commit 0bbf57553c547713de9edf7558f70ed655dab7c6)

--- src/angular-fullscreen.js
+++ src/angular-fullscreen.js
@@ -5,9 +5,6 @@
       module.factory('Fullscreen', ['$document', '$rootScope', function ($document,$rootScope) {
          var document = $document[0];
 
-         // ensure ALLOW_KEYBOARD_INPUT is available and enabled
-         var isKeyboardAvailbleOnFullScreen = (typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element) && Element.ALLOW_KEYBOARD_INPUT;
-
          var emitter = $rootScope.$new();
 
          // listen event on document instead of element to avoid firefox limitation
@@ -24,15 +21,12 @@
             enable: function(element) {
                if(element.requestFullScreen) {
                   element.requestFullScreen();
+               } else if(element.requestFullscreen) {
+                  element.requestFullscreen();
                } else if(element.mozRequestFullScreen) {
                   element.mozRequestFullScreen();
                } else if(element.webkitRequestFullscreen) {
-                  // Safari temporary fix
-                  if (/Version\/[\d]{1,2}(\.[\d]{1,2}){1}(\.(\d){1,2}){0,1} Safari/.test(navigator.userAgent)) {
-                     element.webkitRequestFullscreen();
-                  } else {
-                     element.webkitRequestFullscreen(isKeyboardAvailbleOnFullScreen);
-                  }
+                  element.webkitRequestFullscreen();
                } else if (element.msRequestFullscreen) {
                   element.msRequestFullscreen();
                }
